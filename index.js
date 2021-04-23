905a57e4d40e9adf82cc19e2bf37e77e82f0541d
Corey McCue <coreysmccue@gmail.com>
2021-04-23 15:15:37 +0200

    connect mongoose and prepare dev db

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
