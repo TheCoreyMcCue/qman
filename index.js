d7aace83c31bca9f7e61a53fb6f86494c68f85e6
Corey McCue <coreysmccue@gmail.com>
2021-04-24 09:21:26 +0200

    push user instance to db. Also check for existing user in db so duplicate users aren't created

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-require('./services/passport');
 require('./models/User');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+
+
+// console.log(keys.MongoURI);
 
 mongoose.connect(keys.MongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-  });
+  })
+.then(res => console.log('Connected to DB'))
+.catch(err => console.log('Not Connected to the DB: ', err));
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,7 +12,13 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        // console.log(accessToken);
-        console.log('profile', profile);
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if(existingUser) {
+
+                } else {
+                  new User({ googleId: profile.id }).save();
+                }
+            })
     })
 );
